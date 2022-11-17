8de18c8cfe00337ed71f098e4383e41a5f2062f6
João Pedro Prudêncio <joaopprudencio882@gmail.com>
2022-11-16 23:32:42 -0300

    fix: correct merge to routes

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,56 +1,58 @@
 import { Router } from 'express';
-import listCategoryController from './modules/category/useCases/listCategory';
-import createCategoryController from './modules/category/useCases/createCategory';
-import deleteCategoryController from './modules/category/useCases/deleteCategory';
+
+import DeleteCategoryController from './modules/category/useCases/deleteCategory';
+import CreateCategoryController from './modules/category/useCases/createCategory';
+import ListCategoryController from './modules/category/useCases/listCategory';
+import ListProductController from './modules/product/useCases/listProduct';
 
 export const router = Router();
 
 // List categories
 router.get('/categories', (request, response) =>
-  listCategoryController.handle(request, response),
+  ListCategoryController.handle(request, response),
 );
 
 // Create categories
 router.post('/categories', (request, response) => {
-  createCategoryController.handle(request, response);
+  CreateCategoryController.handle(request, response);
 });
 
 // Delete category
 router.delete('/categories/:categoryId', (request, response) => {
-  deleteCategoryController.handle(request, response);
+  DeleteCategoryController.handle(request, response);
 });
 
 // List products
-router.get('/products', (req, res) => {
-  res.send('OK');
+router.get('/products', (request, response) => {
+  ListProductController.handle(request, response);
 });
 
 // Create products
-router.post('/products', (req, res) => {
-  res.send('OK');
+router.post('/products', (request, response) => {
+  response.send('OK');
 });
 
 // Get products by category
-router.get('/categories/:categoryId/products', (req, res) => {
-  res.send('OK');
+router.get('/categories/:categoryId/products', (request, response) => {
+  response.send('OK');
 });
 
 // List orders
-router.get('/orders', (req, res) => {
-  res.send('OK');
+router.get('/orders', (request, response) => {
+  response.send('OK');
 });
 
 // Create order
-router.post('/orders', (req, res) => {
-  res.send('OK');
+router.post('/orders', (request, response) => {
+  response.send('OK');
 });
 
 // Change order status
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK');
+router.patch('/orders/:orderId', (request, response) => {
+  response.send('OK');
 });
 
 // Delete or Cancel order
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK');
+router.delete('/orders/:orderId', (request, response) => {
+  response.send('OK');
 });
