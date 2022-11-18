9a1a34edbfc521a3049858cc291e3d917d5ca2ce
João Pedro Prudêncio <jprudencio@maeztra.com>
2022-11-17 23:52:15 -0300

    styles: using globalstyles

---
+++ frontend/src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from '../../styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
